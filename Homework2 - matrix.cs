0dd7721556fcdae84128663b18d4088d897bc04e
MIRAN784 <62290259+MIRAN784@users.noreply.github.com>
2020-04-05 17:48:27 +0300

    Add files via upload

---
+++ Homework2 - matrix.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+                static void Main(string[] args)
+        {
+
+
+            // ----------This is matrix string : --------------
+           
+            string[][] matrix1 = new string[3][]
+            {
+                new string[]{"main", "up", "sub"},
+                new string[]{"down", "center" , "up"},
+                new string[]{"sub" , "down" , "main"}
+            };
+
+                   // use foreach loop :
+            Console.WriteLine("This is a matirx of string: ");
+            foreach (string[] Row in matrix1)
+            {
+                foreach (string Col in Row)
+                {
+                    Console.Write(Col + " " );
+                }
+                Console.WriteLine();
+            }
+
+
+            /*
+             * This is a jagged array:
+                main up sub
+                down center up
+                sub down main
+            */
+
+            Console.ReadKey();
+
+        }
+    }
+}
